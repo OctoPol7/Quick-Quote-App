be3fb401c44971b130fc3dcca7842a14ea2eff9e
OctoPol7 <thechumpower@gmail.com>
2022-12-08 12:52:16 -0800

    Initial Commit

---
+++ QuickQuoteApp/Q1ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Q1ViewController.swift
+//  QuickQuoteApp
+//
+//  Created by laptop on 2022-12-08.
+//
+
+import UIKit
+
+class Q1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ QuickQuoteApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  QuickQuoteApp
+//
+//  Created by laptop on 2022-12-08.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "Quick Quotes"
+        
+    }
+    
+
+    
+}
+
